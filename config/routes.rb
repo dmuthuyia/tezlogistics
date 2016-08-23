1e4f2620023a80598bcb883f004c93ef1ba7a73e
dmuthuyia <dmuthuyia@gmail.com>
2016-08-23 17:21:56 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  #devise_for :users
+    devise_for :users
   #devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  #devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   #devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
   #devise_for :users, :controllers => {:sessions => "sessions", :registrations => "registrations",:omniauth_callbacks => "users/omniauth_callbacks"}
   #devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => 'registrations'}
