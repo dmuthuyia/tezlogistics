2137e61e0f1b7817782814cb35d31db0cc67eea7
dmuthuyia <dmuthuyia@gmail.com>
2016-08-24 09:28:33 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  #devise :omniauthable, :omniauth_providers => [:digitalocean]
-  #devise :omniauthable, :omniauth_providers => [:facebook]
+  devise :omniauthable, :omniauth_providers => [:digitalocean]
+  devise :omniauthable, :omniauth_providers => [:facebook]
   #devise :omniauthable, :omniauth_providers => :facebook
   rolify
 
@@ -17,14 +17,14 @@ class User < ActiveRecord::Base
 
 
 
-  #def self.from_omniauth(auth)
-      #where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        #user.provider = auth.provider
-        #user.uid = auth.uid
-        #user.email = auth.info.email
-        #user.password = Devise.friendly_token[0,20]
-      #end
-  #end
+  def self.from_omniauth(auth)
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+      end
+  end
 
     has_attached_file :mypic, :styles => { :medium => "20x20>", :thumb => "10x10>" }, :default_url => "missing.png"
     validates_attachment_content_type :mypic, :content_type => /\Aimage\/.*\Z/
